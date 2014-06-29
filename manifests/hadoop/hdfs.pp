a2065002712da497751d1ef25a61dc2620c27a67
narkisr <narkisr@gmail.com>
2014-06-30 00:18:58 +0300

    dependency re-ordering

--- manifests/hadoop/hdfs.pp
+++ manifests/hadoop/hdfs.pp
@@ -10,27 +10,27 @@ class mesos::hadoop::hdfs(
   ],
   $dfs_name_dir = '/var/lib/hadoop/name'
 ) {
+
   include mesos::hadoop::cloudera
   include cdh::hadoop::datanode
+
+  class {'cdh::hadoop':
+    namenode_hosts     => $namenode_hosts,
+    datanode_mounts    => $datanode_mounts,
+    dfs_name_dir       => $dfs_name_dir,
+  } ->
+
+  file{['/var/lib/hadoop/', '/var/lib/hadoop/data']:
+    ensure  => directory,
+    owner   => 'hdfs',
+    group   => 'hdfs',
+  } ->
 
   file{$datanode_mounts:
     ensure  => directory,
     owner   => 'hdfs',
     group   => 'hdfs',
-    require => File['/var/lib/hadoop/', '/var/lib/hadoop/data']
   } -> Class['cdh::hadoop::datanode']
-
-  file{['/var/lib/hadoop/', '/var/lib/hadoop/data']:
-    ensure => directory,
-    owner  => 'hdfs',
-    group  => 'hdfs'
-  } ->
-
-  class { 'cdh::hadoop':
-    namenode_hosts     => $namenode_hosts,
-    datanode_mounts    => $datanode_mounts,
-    dfs_name_dir       => $dfs_name_dir,
-  }
 
   if($name_node){
     include cdh::hadoop::namenode
